a5fef8d4b24a92d5387dc0f7e1a2b3d6b73403d8
Tien <tien298@gmail.com>
2025-12-22 09:29:48 +0700

    complete controller-error-handling

--- src/modules/history/service-usage.controller.ts
+++ src/modules/history/service-usage.controller.ts
@@ -9,35 +9,23 @@ import { Permissions } from "src/modules/auth/decorators/permissions.decorator";
 import { ApiResponse } from "../../common/response.dto";
 
 @Controller("history_checkin")
-// @ApiBearerAuth("Authorization")
-// @UseGuards(JwtAuthGuard, PermissionsGuard)
+@ApiBearerAuth("Authorization")
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 
 export class ServiceUsageController {
     constructor(
         private readonly serviceUsageService: ServiceUsageService,
     ) { }
     @Get()
-    // @Permissions("Overview.View")
+    @Permissions("Overview.View")
     async getAll(@Query() filter: FillerHistoryDto) {
-        try {
-            const result = await this.serviceUsageService.getHistory(filter);
-            return ApiResponse.ok(result);
-        } catch (error) {
-            return ApiResponse.fail(error.message);
-        }
+        const result = await this.serviceUsageService.getHistory(filter);
+        return ApiResponse.ok(result);
     }
     @Get(':id')
     async getDetail(@Param('id', ParseIntPipe) id: number) {
-        try {
-            const data = await this.serviceUsageService.getDetail(id);
-            return ApiResponse.ok(data, "Lấy chi tiết thành công!");
-        } catch (error) {
-            if (error.status === 404 || error instanceof NotFoundException) {
-                return ApiResponse.fail(`Không tìm thấy cư dân nào có ID là ${id}`);
-            }
-            console.error("Lỗi crash server:", error);
-            return ApiResponse.fail("Lỗi hệ thống, vui lòng thử lại sau");
-        }
+        const data = await this.serviceUsageService.getDetail(id);
+        return ApiResponse.ok(data, "Lấy chi tiết thành công");
     }
 
 }
