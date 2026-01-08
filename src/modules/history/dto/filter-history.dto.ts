bc89010e477c615807b32119eb70d5d863715d8e
phuocly789 <phuoclyminh789@gmai.com>
2026-01-08 11:18:01 +0700

    fix: filter

--- src/modules/history/dto/filter-history.dto.ts
+++ src/modules/history/dto/filter-history.dto.ts
@@ -3,28 +3,33 @@ import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class FillerHistoryDto {
-    @ApiProperty({required:false})
+    @ApiProperty({ required: false })
     @IsOptional()
     @IsString({ message: 'Tên tìm kiếm phải là chuỗi kí tự' })
     searchName?: string; // Tim theo cu dan
 
-     @ApiProperty({required:false})
+    @ApiProperty({ required: false })
     @IsOptional()
     @IsString({ message: 'Tên tìm kiếm phải là chuỗi kí tự' })
     serviceId?: string; // Tim theo dich vu
 
-     @ApiProperty({required:false})
+    @ApiProperty({ required: false })
     @IsOptional()
     @Type(() => Number)
     @IsInt({ message: 'Trang phải là số nguyên' })
     @Min(1, { message: 'Trang phải lớn hơn hoặc bằng 1' })
     page?: number; // Trang so may
 
-     @ApiProperty({required:false})
+    @ApiProperty({ required: false })
     @IsOptional()
     @Type(() => Number)
     @IsInt({ message: 'Giới hạn (limit) phải là số nguyên' })
     @Min(1, { message: 'Limit phải lớn hơn 0' })
     limit?: number;// So ban ghi tren trang
+
+    @ApiProperty({ required: false, enum: ['resident', 'guest'] })
+    @IsOptional()
+    @IsString()
+    type?: 'resident' | 'guest';
 
 }
